ded980a5ce0e55ca52302f31b234b93730d0d531
rochaedgar-max <rocha.edgar@uabc.edu.mx>
2026-04-13 09:48:51 -0700

    feat: Agregué las funciones de actualizar atributos

--- subclases.py
+++ subclases.py
@@ -1,1 +1,31 @@
-# Buenas tardes
+from pokemonconentrenamiento import PokemonConEntrenamiento
+
+
+class PokemonAgua(PokemonConEntrenamiento):
+  def __init__(self):
+     self.ataque_especial = "Hidrobomba"
+
+  def actualizar(self):
+    print(" a ")
+    
+class PokemonFuego(PokemonConEntrenamiento):
+  def __init__(self):
+    self.ataque_especial = "Lanzallamas"
+
+  def actualizar(self):
+    print(" a ")
+    
+class PokemonElectrico(PokemonConEntrenamiento):
+  def __init__(self):
+    self.ataque_especial = "Impactrueno"
+
+  def actualizar(self):
+    print(" a ")
+    
+class PokemonHierba(PokemonConEntrenamiento):
+  def __init__(self):
+    self.ataque_especial = "Látigo Cepa"
+
+  def actualizar(self):
+    print(" a ")
+  
