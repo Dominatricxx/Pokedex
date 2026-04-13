b0bdd7246068f24d766a0863fd82b9dab52a227f
rochaedgar-max <rocha.edgar@uabc.edu.mx>
2026-04-13 08:53:59 -0700

    feat: Agregué la clase Entrenamiento.

--- Entrenamiento.py
+++ Entrenamiento.py
@@ -1,1 +1,14 @@
+from abc import ABC, abstractmethod
 
+class Entrenamiento(ABC):
+  @abstractmethod
+  def subirAtaque():
+    print(" Ataque ")
+
+  @abstractmethod
+  def subirDefensa():
+    print(" Defensa ")
+
+  @abstractmethod
+  def subirVida():
+    print(" Vida ")
