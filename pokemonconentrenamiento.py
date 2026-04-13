a97944f7ffece26addcb1c172e15daa4c95bc5e1
rochaedgar-max <rocha.edgar@uabc.edu.mx>
2026-04-13 09:08:44 -0700

    feat: Agregué la clase PokemonConEntrenamiento.

--- pokemonconentrenamiento.py
+++ pokemonconentrenamiento.py
@@ -1,1 +1,24 @@
 from entrenamiento import Entrenamiento
+from pokemon import Pokemon
+
+class PokemonConEntrenamiento(Entrenamiento, Pokemon):
+  def subirAtaque():
+    if grado == "Basico":
+      ataque += 5
+    elif grado == "Legendario":
+      ataque += 15
+      
+  def subirDefensa():
+    if grado == "Basico":
+      defensa += 5
+    elif grado == "Legendario":
+      defensa += 15
+
+  def subirVida():
+    if grado == "Basico":
+      vida += 5
+    elif grado == "Legendario":
+      vida += 15
+
+
+      
